45a99ed9176200b04169babde0bde8cf76d9e1b8
OzgunEfe <ozgunefe.dev@gmail.com>
2023-05-03 23:36:06 +0100

    I started Carousel section

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../../config/api";
+import { CryptoState } from "../../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 
 const Carousel = () => {
+  const [trending, setTrending] = useState([]);
+  const classes = useStyles();
+
+  const { currency } = CryptoState();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+
+    setTrending(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingCoins();
+  }, [currency]);
+
+  const items = trending.map((coin) => {
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+      </Link>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+
   return (
-    <div>Carousel</div>
-  )
-}
+    <div className={classes.carousel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        responsive={responsive}
+        autoPlay
+        items={items}
+        disableButtonsControls
+      />
+    </div>
+  );
+};
 
 export default Carousel;
