5705e4aa39d7bf06a0ad0b9b9860af09fccfcc06
OzgunEfe <ozgunefe.dev@gmail.com>
2023-05-06 19:34:19 +0100

    Carousel section done.

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -12,13 +12,25 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
   },
+  carouselItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    cursor: "pointer",
+    textTransform: "uppercase",
+    color: "white"
+  },
 }));
+
+export function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
   const classes = useStyles();
 
-  const { currency } = CryptoState();
+  const { currency , symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
@@ -31,6 +43,9 @@ const Carousel = () => {
   }, [currency]);
 
   const items = trending.map((coin) => {
+    let profit = coin.price_change_percentage_24h >= 0;
+
+
     return (
       <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
         <img
@@ -39,6 +54,20 @@ const Carousel = () => {
           height="80"
           style={{ marginBottom: 10 }}
         />
+        <span>
+            {coin?.symbol}
+            &nbsp;
+            <span style={{
+                color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                fontWeight: 500,
+            }}>
+                {profit && '+'} {coin.price_change_percentage_24h?.toFixed(2)}%
+            </span>
+        </span>
+
+        <span style={ {fontSize: 22, fontWeight:500 }} >
+            {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
       </Link>
     );
   });
